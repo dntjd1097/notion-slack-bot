0a9c5bd92fe42007386205ab2557a6918eda3752
dntjd1097 <dntjd1097@gmail.com>
2023-04-04 11:10:55 +0900

    fix : bug

--- notion.py
+++ notion.py
@@ -1,10 +1,7 @@
-import requests, json
-from mee6_py_api import API
 import requests
 from configure import api_key as api
 from dateutil import parser
-from dateutil import parser
-from datetime import date, datetime, timezone, timedelta
+from datetime import datetime, timezone, timedelta
  
 def post_message( channel,title,start_date,end_date,position):
     """슬랙 메시지 전송"""
@@ -60,8 +57,6 @@ def readDatabase(databaseId, ):
                 position=""
             if(not end_date):
                 end_date=""
-                #print(end_date)
-            #print(e,title,start_date,end_date)
         except Exception:
             pass
             
@@ -69,34 +64,31 @@ def readDatabase(databaseId, ):
         
         if (title!="" and start_date!=""):
             try:
-                back=start_date[:10]
+                check=start_date[:10]
                 #시간 계산
-                if((start_date==tommorrow or back == tommorrow)
-                   or(start_date==today or back == today)
-                   or(start_date==week or back == week)
+                if((start_date==tommorrow or check == tommorrow)
+                   or(start_date==today or check == today)
+                   or(start_date==week or check == week)
                    ):
                     #시작시간 = 2023-04-05
                     if(start_date ==start_date[:10]):
-                        #print("?얘 머야?",title)
-                        #end_date= " ~ "+end_date
+                        if(end_date==start_date):
+                            end_date=""
+                        
+                        if(end_date!=""):
+                            end_date=" ~ "+end_date
                         post_message("#validator",title,start_date,end_date,position)
-                    
                     else:
                         start_date=start_date[:16]
                         start_date=datetime.strptime(start_date, '%Y-%m-%dT%H:%M')
                         start_date=start_date.strftime('%Y-%m-%d %I:%M %p')
                         if(end_date!=""):
-                            type(end_date)
                             end_date=end_date[:16]
                             end_date=datetime.strptime(end_date, '%Y-%m-%dT%H:%M')
                             end_date=" ~ "+str(end_date.strftime('%Y-%m-%d %I:%M %p'))
-
                         post_message("#validator",title,start_date,end_date,position)
-                
-                back=""    
+                check=""    
             except TypeError as e:
                 pass
                 
-            
-               
 readDatabase(api["databaseId"])
